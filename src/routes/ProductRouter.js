e2f93129cb0d3250bd0cb0629ac44260f6c55cf5
Huy <dinhphongtamquoc453@gmail.com>
2024-03-28 20:53:38 +0700

    Fix tý

--- src/routes/ProductRouter.js
+++ src/routes/ProductRouter.js
@@ -10,6 +10,7 @@ router.get('/details/:id' ,ProductController.getDetailsProduct)
 router.get('/getAllProduct' ,ProductController.getAllProduct)
 router.delete('/delete-product/:id',checkDeleteProduct,authMiddleWare ,ProductController.deleteProduct)
 router.delete('/delete-many',authMiddleWare ,ProductController.deleteMany)
+router.get('/get-all-type' ,ProductController.getAllType)
 
 
 module.exports = router
